18a9c81c4ced651bc8da92c1cd7af0fdd8de8b06
MrD <the.ubik@gmail.com>
2020-12-13 15:05:54 +0200

    XXX debug XXX

--- Mopy/bash/brec/basic_elements.py
+++ Mopy/bash/brec/basic_elements.py
@@ -24,6 +24,7 @@
 higher-level building blocks can be found in common_subrecords.py."""
 from typing import BinaryIO
 
+from collections import Counter
 from itertools import chain
 
 from .utils_constants import FID, null1, _make_hashable, FixedString, \
@@ -36,6 +37,8 @@ from ..bolt import decoder, encode, structs_cache, struct_calcsize, Rounder, \
 class MelObject(object):
     """An empty class used by group and structure elements for data storage."""
     __slots__ = ()
+    _cache_misses = Counter()
+    _key_errors = Counter()
 
     def __eq__(self,other):
         """Operator: =="""
@@ -257,6 +260,14 @@ class MelBase(Subrecord):
         :rtype: int"""
         raise exception.AbstractError()
 
+    def __repr__(self):
+        return u'[%s]: %s' % (type(self).__name__, getattr(self, u'attr', None))
+
+class MelCollection(MelBase):
+    """Any old collection of mod elements."""
+    def __init__(self, mel_sig, attr=u'', *elements):
+        super(MelCollection, self).__init__(mel_sig, attr)
+
 # Simple static Fields --------------------------------------------------------
 class _MelNum(MelBase):
     """A simple static subrecord representing a number."""
@@ -424,6 +435,7 @@ class MelGroup(MelSequential):
                        u'.' not in m)))
             mel_set_obj = group_mel_set
             def __getattr__(self, missing_attr, __mset=mel_set_obj):
+                self.__class__._cache_misses[missing_attr] += 1
                 if missing_attr in __mset.defaulters:
                     target = __mset.defaulters[missing_attr]
                 elif missing_attr in __mset.listers:
@@ -431,6 +443,10 @@ class MelGroup(MelSequential):
                 elif missing_attr in __mset.mel_providers_dict:
                     target = __mset.mel_providers_dict[missing_attr]()
                 else:
+                    if not missing_attr in self.__class__._key_errors:
+                        # https://stackoverflow.com/a/33388198/281545
+                        print(missing_attr)  # '__deepcopy__' !
+                    self.__class__._key_errors[missing_attr] += 1
                     raise AttributeError(missing_attr)
                 setattr(self, missing_attr, target)
                 return target
--- Mopy/bash/brec/record_structs.py
+++ Mopy/bash/brec/record_structs.py
@@ -27,6 +27,7 @@ from collections import defaultdict
 import copy
 import io
 import zlib
+from collections import Counter
 
 from .basic_elements import SubrecordBlob, unpackSubHeader
 from .mod_io import ModReader
@@ -565,6 +566,8 @@ class MelRecord(MreRecord):
     # subrecord. See MelSet.check_duplicate_attrs for more information.
     _has_duplicate_attrs = False
     __slots__ = []
+    _cache_misses = Counter()
+    _key_errors = Counter()
 
     def __init__(self, header, ins=None, do_unpack=False):
         if self.__class__.rec_sig != header.recType:
@@ -632,6 +635,7 @@ class MelRecord(MreRecord):
         self.__class__.melSet.updateMasters(self, masterset_add)
 
     def __getattr__(self, missing_attr):
+        self.__class__._cache_misses[missing_attr] += 1
         if missing_attr in self.__class__.melSet.defaulters:
             target = self.__class__.melSet.defaulters[missing_attr]
         elif missing_attr in self.__class__.melSet.listers:
@@ -639,6 +643,10 @@ class MelRecord(MreRecord):
         elif missing_attr in self.__class__.melSet.mel_providers_dict:
             target = self.__class__.melSet.mel_providers_dict[missing_attr]()
         else:
+            if not missing_attr in self.__class__._key_errors:
+                # https://stackoverflow.com/a/33388198/281545
+                print (missing_attr) # '__deepcopy__' !
+            self.__class__._key_errors[missing_attr] += 1
             raise AttributeError(missing_attr)
         setattr(self, missing_attr, target)
         return target
